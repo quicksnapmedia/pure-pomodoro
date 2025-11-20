39062946d32adea5801cc86afb56bafc52ace3d6
Colin Kiama <colinkiama@gmail.com>
2025-11-20 21:20:27 +0000

    Add unstyled index page content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,18 @@
 <template>
+	<header>
+		<span class="logo-placeholder"></span>
+		<span class="placeholder-title">PurePomodoro</span>
+	</header>
+  <p class="time">11:11</p>
+  <div class="timer-controls">
+  	<button>Reset</button>
+  	<button>Play</button>
+  </div>
+
+  <div>
+  	<span class="feature-title">Sessions Completed</span>
+  	<p class="feature-text">0/4</p>
+  </div>
   <footer>
   <ul>
     <li><NuxtLink to="/settings">Settings</NuxtLink></li>
